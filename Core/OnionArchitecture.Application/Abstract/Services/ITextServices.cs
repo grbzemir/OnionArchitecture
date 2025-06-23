b757cf93ca30cc422195f2b229c9c3b113e480cf
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 23:45:36 +0300

    Interfaces

---
+++ Core/OnionArchitecture.Application/Abstract/Services/ITextServices.cs
@@ -0,0 +1,14 @@
+﻿using OnionArchitecture.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Application.Abstract.Services
+{
+    public interface ITextServices
+    {
+        string FormatTextForEvent(IEnumerable<EventDto> evenItems);
+    }
+}
