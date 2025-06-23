1d3eeb57605d0ceee989b668975f11dc2244e138
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 21:22:41 +0300

    Interfaces

---
+++ Core/OnionArchitecture.Application/Abstract/Services/IEventService.cs
@@ -0,0 +1,21 @@
+﻿using OnionArchitecture.Application.Dtos;
+using OnionArchitecture.Application.RequestParameters;
+using OnionArchitecture.Domain.Entities;
+using OnionArchitecture.Domain.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Application.Abstract.Services
+{
+    public interface IEventService
+    {
+        Task CreateEventAsync(CreateEventDto createEventDto);
+        Task<IEnumerable<EventDto>> GetAllEventsAsync(Pagination pagination);
+
+       
+
+    }
+}
