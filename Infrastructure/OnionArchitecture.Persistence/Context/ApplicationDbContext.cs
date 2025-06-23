71d80c0e3261faf28b27a7484ecf98f86e193598
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 21:23:05 +0300

    DbContext

---
+++ Infrastructure/OnionArchitecture.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Domain.Commen;
+using OnionArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Persistence.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("OnionArchitectureDb");
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var data = ChangeTracker.Entries<EntityBase>();
+
+            foreach (var entry in data)
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Entity.CreatedDate = DateTime.UtcNow;
+
+                else if (entry.State == EntityState.Modified)
+                    entry.Entity.UpdatedDate = DateTime.UtcNow;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
