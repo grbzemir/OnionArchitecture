37b1130ad60e2c07166e4a041dc03f266b597084
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 23:46:09 +0300

    Infrastructure

---
+++ Infrastructure/OnionArchitecture.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnionArchitecture.Application.Abstract.Services;
+using OnionArchitecture.Infrastructure.Services;
+
+namespace OnionArchitecture.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IFileService, FileService>();
+            services.AddScoped<ITextServices, TextService>();
+        }
+    }
+}
