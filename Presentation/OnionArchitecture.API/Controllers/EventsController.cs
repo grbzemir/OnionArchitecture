202aa1458da2b978fcf32a2c61bb2b1c0902c65d
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 23:46:45 +0300

    Controllers

---
+++ Presentation/OnionArchitecture.API/Controllers/EventsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionArchitecture.Application.Abstract.Services;
+using OnionArchitecture.Application.Concrete;
+using OnionArchitecture.Application.Dtos;
+using OnionArchitecture.Application.RequestParameters;
+using System.ComponentModel.Design;
+
+namespace OnionArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+        private readonly ExportService _exportService;
+        public EventsController(IEventService eventService , ExportService exportService)
+        {
+            _eventService = eventService;
+            _exportService = exportService;
+
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllEvents([FromQuery] Pagination pagination)
+        {
+            var events = await _eventService.GetAllEventsAsync(pagination);
+            return Ok(events);
+        }
+
+        [HttpPost("[action]")]
+
+        public async Task<IActionResult> CreateEvent([FromBody] CreateEventDto createEventDto)
+        {
+            if (createEventDto == null)
+            {
+                return BadRequest("Invalid event data.");
+            }
+
+            await _eventService.CreateEventAsync(createEventDto);
+
+            return Ok();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> ExportEvents([FromQuery] Pagination pagination, string path)
+        {
+            var events = await _eventService.GetAllEventsAsync(pagination);
+
+            await _exportService.ExportEventsAsync(events, path);
+
+            return Ok();
+        }
+    }
+}
--- Presentation/OnionArchitecture.API/Program.cs
+++ Presentation/OnionArchitecture.API/Program.cs
@@ -1,3 +1,10 @@
+using OnionArchitecture.Application.Abstract.Services;
+using OnionArchitecture.Application.Concrete;
+using OnionArchitecture.Persistence.Services;
+using OnionArchitecture.Persistence;
+using OnionArchitecture.Application;
+using OnionArchitecture.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IEventService , EventServices>();
+builder.Services.AddScoped<ExportService>();
+
+builder.Services.AddPersistenceServices();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
 
 var app = builder.Build();
 
