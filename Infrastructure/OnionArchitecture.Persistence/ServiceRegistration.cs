06e393d31443449b3012049135435ace593a2cf3
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 23:46:35 +0300

    Service

---
+++ Infrastructure/OnionArchitecture.Persistence/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnionArchitecture.Application.Abstract.Services;
+using OnionArchitecture.Application.Concrete;
+using OnionArchitecture.Persistence.Context;
+using OnionArchitecture.Persistence.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddScoped<IEventService, EventServices>();
+            services.AddDbContext<ApplicationDbContext>();
+        }
+    }
+}
